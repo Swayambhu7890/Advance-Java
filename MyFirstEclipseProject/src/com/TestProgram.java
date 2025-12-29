8a72b93417f005788c4c6634de9b0dfc0b858d3a
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2025-12-29 18:26:30 +0530

    My first Eclipse Code

---
+++ MyFirstEclipseProject/src/com/TestProgram.java
@@ -0,0 +1,14 @@
+package com;
+
+public class TestProgram 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("My IDE Working Fine");
+
+	}
+
+}
