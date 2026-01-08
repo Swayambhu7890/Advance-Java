e74afcfa45fdb4dc547ab42cebbd4e6cc9664d56
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2026-01-08 19:06:53 +0530

    Session Tracking Mechanism:- 3)URL Rewriting done

---
+++ HttpSessionDemo/src/main/java/com/ServletOne.java
@@ -0,0 +1,61 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletOne
+ */
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+			response.setContentType("text/html");
+			
+			PrintWriter pw = response.getWriter();
+			String s = request.getParameter("username");
+			pw.println("Welcome "+s);
+			
+			HttpSession hs = request.getSession();                          //To generate the session id in a container
+			hs.setAttribute("sname", s);
+			
+			pw.println("<a href = 'ServletTwo'> Visit</a>");
+			pw.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
