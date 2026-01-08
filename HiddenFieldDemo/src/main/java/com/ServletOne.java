4c6fdf46a89fa8df0bf47e052b637e428971fe2a
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2026-01-08 19:03:19 +0530

    Session Tracking Mechanism:-2) Hidden Form Field  done

---
+++ HiddenFieldDemo/src/main/java/com/ServletOne.java
@@ -0,0 +1,60 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ServletOne
+ */
+@WebServlet("/ServletOne")
+public class ServletOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOne() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		try
+		{
+			response.setContentType("text/html");
+			
+			PrintWriter pw = response.getWriter();
+			String s = request.getParameter("username");
+			pw.println("Welcome "+s);
+			
+			pw.println("<form action = 'ServletTwo' method = 'get'>"); 
+			pw.println("<input type = 'hidden' name = 'hfield' value = ' "+s+" '>");
+			pw.println("<input type = 'submit' value = 'Visit'>");
+			pw.println("</form>");
+			
+			pw.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
