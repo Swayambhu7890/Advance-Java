2d003cd162abe3c7a5d91dd044f8fd6e9340f14c
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2026-01-29 00:20:47 +0530

    How to create  a basic Hibernate Program done

---
+++ HibernateDemo/src/main/java/test/PersonDao.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class PersonDao 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		//Step:-1 Reading details from hibernate configuration file(Read the information, Establish the connection, create query, execute query,Table Created)
+		Configuration conf=new Configuration();
+		conf.configure("hibernate.cfg.xml");
+		
+		//If to perform operations on Table
+		//Step:2 Allow to create a new session (To establish a new session with the database)
+		
+		SessionFactory factory=conf.buildSessionFactory();
+		
+		//Step:-3 To start the session
+		
+		Session session=factory.openSession();
+		
+		//Step:-4 Begin the Transactions to perform operations on table
+		
+		Transaction tx=session.beginTransaction();
+		
+		//To call the Employee class constructor
+		Person p=new Person("Pratham", 20, "Pune", 50000);
+		
+		//To make the persistent object
+		session.save(p);                                   
+		
+		//save the transactions done
+		tx.commit();
+		session.close();
+		factory.close();
+		
+		System.out.println("Details Added Successfully");
+		
+     }
+}
