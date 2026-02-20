15039cb0219e46ce4181f4167744744c94983f65
Swayambhu7890 <bhapkarswayambhu@gmail.com>
2026-02-20 22:58:32 +0530

    To add the annotation to list element, How to inject the data for referenced type of element, Spring Expression Language(Which is only used with the @Value Annotation), Expression Language using Boolean DataType.

--- SpringAnnotationDemo/src/com/config/annotations/App.java
+++ SpringAnnotationDemo/src/com/config/annotations/App.java
@@ -16,6 +16,14 @@ public class App {
 		System.out.println(instructor2.hashCode());
 		
 		//Above two objects are working in same scope as their hashCode values are same
+		
+		//How to use Annotations for List 
+		Instructor instructorlist = (Instructor)context.getBean("inst");
+		System.out.println(instructorlist);
+		
+		//To use the String Predefined Methods
+		String name = "Rahul";
+		System.out.println(name.charAt(0));
 	}
 
 }
--- SpringAnnotationDemo/src/com/config/annotations/Instructor.java
+++ SpringAnnotationDemo/src/com/config/annotations/Instructor.java
@@ -1,5 +1,8 @@
 package com.config.annotations;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -8,11 +11,31 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")                         //To change the scope(hashCode) of object(Scope Annotation :- To define scope of object)
 public class Instructor 
 {
-	@Value("101")
-    private int id;
+	//@Value("101")
+	//@Value("#{4 + 2}")                                 //Expression language with primitive types for mathematical operation
+	//@Value("#{4>6?25:20}")                             //Expression language with primitive types for relational  operation
+	//@Value("#{T(java.lang.Math).abs(-99)}")            //Expression language using Static method for Absolute method
+	//@Value("#{new Integer(88)}")                       //Expression language using Static method for Object type
+	//@Value("#{T(java.lang.Integer).MIN_VALUE}")        //Expression language using Static method for Constant value
+	@Value("#{T(java.lang.Integer).MAX_VALUE}")          //Expression language using Static method for Constant value
+    private int id = 101;
 	
-	@Value("Swayambhu")
-    private String name;
+	//@Value("Swayambhu")
+	//@Value("#{'Swayambhu Bhapkar'}")                              //Expression language with String types
+	//@Value("#{new java.lang.String('Swayambhu Bhapkar')}")        //Expression language with String type to define it as a object   
+	//@Value("#{'Swayambhu Bhapkar'.toUpperCase()}")                //Expression language with String methods in UpperCase 
+	@Value("#{'Swayambhu Bhapkar'.toLowerCase()}")                  //Expression language with String methods in LowerCase
+    private String name = "Ram";
+	
+	@Value("#{topics}")                                             //Expression Language
+	private List<String> topics;                                    //To inject the list using annotation
+	
+	@Autowired                                                      //To Inject Referenced type of dependency(data)  
+	private Profile profile;
+	
+	//@Value("#{2+4<5}")
+	@Value("#{2+4<5?false:true}")
+	private boolean active;                                         //Expression Language using Boolean DataType                    
     
 	public int getId() 
 	{
@@ -34,8 +57,14 @@ public class Instructor
 	@Override
 	public String toString() 
 	{
-		return "Instructor [id=" + id + ", name=" + name + "]";
+		return "Instructor [id=" + id + ", name=" + name + ", topics=" + topics + ", profile=" + profile + ", active=" + active + "]";
 	}
+	
+	
+	
+	
+	
+	
 	
 	   
 }
